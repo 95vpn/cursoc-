7e3ddbd25485ac7d1b1ce3af03957f3b2cf2d3ae
Johnny Herrera <jmjaramillo95@gmail.com>
2025-06-02 22:30:38 -0500

    Agregar archivos de proyecto.

---
+++ cursoc#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cursoc_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 10;
+            Console.WriteLine("Hello world" + year);
+
+            Console.ReadLine(); 
+        }
+    }
+}
