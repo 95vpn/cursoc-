45876bfd20b564b24483d951c19eaf7609b44d40
Johnny Herrera <jmjaramillo95@gmail.com>
2025-06-04 22:32:22 -0500

    condicionales

--- cursoc#/Program.cs
+++ cursoc#/Program.cs
@@ -10,6 +10,9 @@ namespace cursoc_
     {
         static void Main(string[] args)
         {
+            /*Decimales*/
+
+            /*
             int year = Convert.ToInt16("52");
             double doble = 2.2;
 
@@ -19,9 +22,73 @@ namespace cursoc_
 
             decimal decimals = 2.07M;
             float flotante = 8.3F;
+
+            bool valor = true;
+
+            char letra = 'A';
+
+            Decimal de = 28.6M;
+            Boolean valor2 = false;
+            
             Console.WriteLine(nombres + "Hello world! {0} {1} {2} {3}", year, doble, nombre, decimals);
 
             Console.ReadLine(); 
+            */
+
+            /*
+            int valor1 = 3;
+            int valor2 = 4;
+            int valor3 = 5;
+            int valor4 = 2;
+            decimal valor5 = 3M;
+            decimal valor6 = 7M;
+
+            decimal valor7 = 3M;
+            decimal valor8 = 4M;
+
+
+            //var suma = valor1 + valor2
+            valor1 += valor2;
+            valor2 += valor1;
+
+            valor3 -= valor4;
+            valor4 -= valor3;
+
+            valor5 /= valor6;
+            valor6 /= valor5;
+
+            valor7 %= valor8;
+            valor8 %= valor7;
+            Console.WriteLine("Resultado {0} {1} {2} {3} {4} {5} {6} {7}", valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8);
+            Console.ReadLine();
+            */
+
+            /*
+            int valor1 = 0;
+            int valor2 = 2;
+
+            var data = valor1 == valor2;
+            Console.WriteLine("Resultado {0}", data);
+
+            Console.ReadLine();
+            */
+
+            int valor1 = 9;
+            int valor2 = 8;
+
+            double valor3 = 10;
+            double valor4 = 10; 
+
+            var data = valor1 != valor2;
+
+            if (valor1 != valor2 || valor3 == valor4)
+            {
+                Console.WriteLine("Resultado {0}", data);
+
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
