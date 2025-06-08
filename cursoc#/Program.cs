426e091252217bf1d392a8d210d508fd8ec692ec
Johnny Herrera <jmjaramillo95@gmail.com>
2025-06-07 23:22:40 -0500

    pascal

--- cursoc#/Program.cs
+++ cursoc#/Program.cs
@@ -141,14 +141,15 @@ namespace cursoc_
             Console.ReadLine();
             */
 
+            /*
             string[] name = { "Alex", "Joel", "Pdhn" };
             int[] age = { 52, 45, 65 };
-            /*
+            
             for (int i = 0; i < name.Length; i++)
             {
                 Console.WriteLine("Resultado {0} ", name[i] + " age " + age[i]);
             }
-            */
+            
             Console.ReadLine();
 
             foreach (var item in name)
@@ -156,6 +157,40 @@ namespace cursoc_
                 Console.WriteLine("Resultado {0}", item);
             }
 
+            Console.ReadLine();
+            */
+
+            int pisos = 0;
+            int[] arreglo = new int[1];
+
+            Console.WriteLine("Ingrese el numero de pisos");
+            pisos = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 1; i <= pisos; i++)
+            {
+                int[] pascal = new int[i];
+
+                for (int j = pisos; j < i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < i; k++)
+                {
+                    if(k == 0 || k == (i -1))
+                    {
+                        pascal[k] = 1;
+                    }
+                    else
+                    {
+                        pascal[k] = arreglo[k] + arreglo[k - 1];
+                    }
+                    Console.Write("[" + pascal[k] + "]");   
+                }
+                arreglo = pascal;
+                Console.WriteLine(" ");
+
+            }
             Console.ReadLine();
 
         }
